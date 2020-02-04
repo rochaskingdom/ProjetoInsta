3028c938da0e677d896326a19b2f39d712bb9339
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-04 13:16:17 -0300

    Componente Comentários

--- App.js
+++ App.js
@@ -6,10 +6,16 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
 	render() {
+		const comments = [{
+			nickname: 'Joana Elena Silva',
+			comment: 'Excelente Foto!'
+		}]
+
 		return (
 			<View style={{ flex: 1 }}>
 				<Header />
-				<Post image={require('./assets/imgs/fence.jpg')} />
+				<Post image={require('./assets/imgs/fence.jpg')}
+					comments={comments} />
 			</View>
 		)
 	}
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Image, Dimensions } from 'react-native'
 
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render() {
@@ -9,6 +10,7 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='fulano@gmail.com' nickname='Fulano de Tal' />
+                <Comments comments={this.props.comments} />
             </View>
         )
     }
