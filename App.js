96e27773424c1e23d62b5f25234c8240cad61611
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-04 12:53:58 -0300

    Componente Autor

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import Header from './src/components/Header'
+import { View, Text } from 'react-native'
+
+import Post from './src/components/Post'
 
 export default class App extends Component {
 	render() {
 		return (
-			<Header />
+			<View style={{ flex: 1 }}>
+				<Header />
+				<Post image={require('./assets/imgs/fence.jpg')} />
+			</View>
 		)
 	}
 }
---
+++ src/components/Post.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Image, Dimensions } from 'react-native'
+
+import Author from './Author'
+
+class Post extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={this.props.image} style={styles.image} />
+                <Author email='fulano@gmail.com' nickname='Fulano de Tal' />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3 / 4,
+        resizeMode: 'contain'
+    }
+})
+
+export default Post
